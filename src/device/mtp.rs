cf4ac94e903f66bcc79426ce022d5ae9bdaa7898
daladim <daladim@users.noreply.github.com>
2023-05-19 23:09:05 +0200

    Implemented traits for MTP storage

--- src/device/mtp.rs
+++ src/device/mtp.rs
@@ -7,11 +7,12 @@
 // TODO: revert
 #![allow(unused_variables)]
 
-use std::path::Path;
+use std::ffi::OsStr;
+use std::path::{Path, PathBuf};
 use std::error::Error;
 use std::io::Read;
 
-use winmtp::Provider;
+use winmtp::{Provider, error::ItemByPathError};
 use winmtp::device::BasicDevice;
 use winmtp::device::device_values::AppIdentifiers;
 use winmtp::object::Object;
@@ -48,8 +49,31 @@ pub fn devices() -> Vec<RootObject> {
 }
 
 pub struct RootObject(BasicDevice, Object);
-pub struct FolderObject(Object);
-pub struct FileObject(Object);
+pub struct FolderObject(Object, PathBuf);
+pub struct FileObject(Object, PathBuf);
+
+impl RootObject {
+    fn starsync_folder_impl(&self) -> Result<(Object, PathBuf), ItemByPathError> {
+        let path = crate::device::FOLDER_NAME;
+        self.1
+            .object_by_path(&Path::new(path))
+            .map(|o| (o, PathBuf::from(path)))
+    }
+
+    fn config_folder_impl(&self) -> Result<(Object, PathBuf), ItemByPathError> {
+        let path = Path::new(crate::device::FOLDER_NAME).join(crate::device::CONFIG_FOLDER_NAME);
+        self.1
+            .object_by_path(&path)
+            .map(|o| (o, path))
+    }
+
+    fn music_folder_impl(&self) -> Result<(Object, PathBuf), ItemByPathError> {
+        let path = Path::new(crate::device::FOLDER_NAME).join(crate::device::MUSIC_FOLDER_NAME);
+        self.1
+            .object_by_path(&path)
+            .map(|o| (o, path))
+    }
+}
 
 impl super::Device for RootObject {
     fn name(&self) -> String {
@@ -60,86 +84,180 @@ impl super::Device for RootObject {
     }
 
     fn starsync_folder(&self) -> Option<Box<dyn Folder>> {
-        self.1
-            .object_by_path(Path::new("StarSync"))
-            .map(|obj| Box::new(FolderObject(obj)) as Box<dyn Folder>)
+        self.starsync_folder_impl()
+            .map(|obj| Box::new(FolderObject(obj.0, obj.1)) as Box<dyn Folder>)
             .ok()
     }
 
     fn config_folder(&self) -> Option<Box<dyn Folder>> {
-        todo!();
+        self.config_folder_impl()
+            .map(|obj| Box::new(FolderObject(obj.0, obj.1)) as Box<dyn Folder>)
+            .ok()
     }
 
     fn music_folder(&self) -> Option<Box<dyn Folder>> {
-        todo!();
+        self.music_folder_impl()
+            .map(|obj| Box::new(FolderObject(obj.0, obj.1)) as Box<dyn Folder>)
+            .ok()
     }
 
     fn create_folders(&self) -> Result<(), Box<dyn Error>> {
-        todo!();
+        // Does the root folder even exist?
+        if self.1.sub_folders()?.any(|folder| &folder.name().to_os_string() == &OsStr::new(crate::device::FOLDER_NAME)) == false {
+            self.1
+                .create_subfolder(&OsStr::new(crate::device::FOLDER_NAME))?;
+        }
+        let starsync_folder = self.1.object_by_path(&Path::new(crate::device::FOLDER_NAME))?;
+
+        let mut has_music = false;
+        let mut has_config = false;
+
+        for existing_sub in starsync_folder.sub_folders()? {
+            let existing_sub_name = existing_sub.name().to_string_lossy().to_string();
+            if &existing_sub_name == crate::device::MUSIC_FOLDER_NAME {
+                has_music = true;
+            }
+            if &existing_sub_name == crate::device::CONFIG_FOLDER_NAME {
+                has_config = true;
+            }
+        }
+
+        if has_music == false {
+            starsync_folder.create_subfolder(&OsStr::new(crate::device::MUSIC_FOLDER_NAME))?;
+        }
+        if has_config == false {
+            starsync_folder.create_subfolder(&OsStr::new(crate::device::CONFIG_FOLDER_NAME))?;
+        }
+
+        Ok(())
     }
 
     fn remove_folders(&self) -> Result<(), Box<dyn Error>> {
-        todo!();
+        self.starsync_folder_impl()?
+            .0.delete(true)?;
+        Ok(())
     }
 
     fn config_display_path(&self) -> String {
-        todo!();
+        format!("{}/{}",
+            self.name(),
+            crate::device::CONFIG_FILE
+        )
     }
 
     fn config(&self) -> Option<Config> {
-        todo!();
+        let config_file = self
+            .config_folder_impl()
+            .ok()?
+            .0
+            .object_by_path(&Path::new(crate::device::CONFIG_FILE))
+            .ok()?
+            .open_read_stream()
+            .ok()?;
+        serde_json::from_reader(config_file).ok()
     }
 
     fn push_config(&self, config: &Config) -> Result<(), Box<dyn Error>> {
-        todo!();
+        let config_json = serde_json::to_string_pretty(&config).map_err(|err| format!("Unable to serialize the configuration: {}", err))?;
+
+        Ok(self.config_folder_impl()?
+            .0
+            .push_data(&OsStr::new(crate::device::CONFIG_FILE), config_json.as_bytes(), true)?)
     }
 
     fn previous_sync_infos(&self) -> Option<SyncInfo> {
-        todo!();
+        let reader = self.config_folder_impl()
+            .ok()?
+            .0
+            .object_by_path(&Path::new(crate::device::SYNC_INFO_FILE))
+            .ok()?
+            .open_read_stream()
+            .ok()?;
+
+        serde_json::from_reader(reader).ok()
     }
 
     fn push_sync_infos(&self, sync_infos: &SyncInfo) -> Result<(), Box<dyn Error>> {
-        todo!();
+        let info_json = serde_json::to_string_pretty(&sync_infos).map_err(|err| format!("Unable to serialize the sync info: {}", err))?;
+
+        Ok(self.config_folder_impl()?
+            .0
+            .push_data(&OsStr::new(crate::device::SYNC_INFO_FILE), info_json.as_bytes(), true)?)
     }
 
     fn push_music_file(&self, local_absolute_path: &Path, device_relative_path: &Path) -> Result<(), Box<dyn Error>> {
-        todo!();
+        let device_folder_path = device_relative_path.parent().ok_or(format!("Path has no parent folder"))?;
+
+        // Create the parent dir, if needed
+        self
+            .music_folder_impl()?
+            .0
+            .create_subfolder_recursive(device_folder_path)?;
+
+        let device_folder = self.music_folder_impl()?.0.object_by_path(device_folder_path)?;
+        device_folder.push_file(local_absolute_path, true)?;
+
+        Ok(())
     }
 
-    fn push_playlist(&self, content: &str, playlist_name: &Path) -> Result<(), Box<dyn Error>> {
-        todo!();
+    fn push_playlist(&self, content: &str, playlist_name: &OsStr) -> Result<(), Box<dyn Error>> {
+        Ok(self.starsync_folder_impl()?
+            .0
+            .push_data(playlist_name, content.as_bytes(), true)?)
     }
 }
 
 impl Folder for FolderObject {
     fn path(&self) -> &Path {
-        todo!();
+        &self.1
     }
 
     fn sub_folders(&self) -> Result<Vec<Box<dyn Folder>>, Box<dyn Error>> {
-        todo!();
+        Ok(self
+            .0
+            .sub_folders()?
+            .map(|obj| {
+                let sub_path = obj.name().to_os_string();
+                let path = self.1.join(sub_path);
+                Box::new(FolderObject(obj, path)) as Box<dyn Folder>
+            })
+            .collect())
     }
 
     fn files(&self) -> Result<Vec<Box<dyn File>>, Box<dyn Error>> {
-        todo!();
+        Ok(self
+            .0
+            .children()?
+            .filter(|obj| obj.object_type().is_file_like())
+            .map(|obj| {
+                let sub_path = obj.name().to_os_string();
+                let path = self.1.join(sub_path);
+                Box::new(FileObject(obj, path)) as Box<dyn File>
+            })
+            .collect())
     }
 
     fn file_at(&self, relative_path: &Path) -> Result<Box<dyn File>, Box<dyn Error>> {
-        todo!();
+        Ok(self
+            .0
+            .object_by_path(relative_path)
+            .map(|obj| Box::new(FileObject(obj, PathBuf::from(relative_path))) as Box<dyn File>)?)
     }
 }
 
 impl File for FileObject {
     fn path(&self) -> &Path {
-        todo!();
+        &self.1
     }
 
     fn get_reader(&self) -> Result<Box<dyn Read>, Box<dyn Error>> {
-        todo!();
+        let reader = self.0.open_read_stream()?;
+        Ok(Box::new(reader) as Box<dyn Read>)
     }
 
-    fn delete(&self) -> Result<(), Box<dyn Error>> {
-        todo!();
+    fn delete(&mut self) -> Result<(), Box<dyn Error>> {
+        self.0.delete(false)?;
+        Ok(())
     }
 }
 
