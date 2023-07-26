d3d90bf68cb651e8e3bbc8017adf63aa0544b294
daladim <daladim@users.noreply.github.com>
2023-07-26 20:44:57 +0200

    Disable sleep during a sync

--- src/lib.rs
+++ src/lib.rs
@@ -74,3 +74,30 @@ pub fn deinit_device(device_name: &str) -> Result<(), DeinitError> {
     Ok(())
 }
 
+
+/// A RAII wrapper to tell Windows not to go to sleep while a sync is in progress
+pub struct PleaseStayAwake {}
+
+impl PleaseStayAwake {
+    pub fn new() -> Self {
+        unsafe{
+            windows::Win32::System::Power::SetThreadExecutionState(
+                windows::Win32::System::Power::ES_CONTINUOUS |
+                // windows::Win32::System::Power::ES_AWAYMODE_REQUIRED |
+                windows::Win32::System::Power::ES_SYSTEM_REQUIRED
+            );
+        }
+
+        Self {}
+    }
+}
+
+impl Drop for PleaseStayAwake {
+    fn drop(&mut self) {
+        unsafe{
+            windows::Win32::System::Power::SetThreadExecutionState(
+                windows::Win32::System::Power::ES_CONTINUOUS
+            );
+        }
+    }
+}
